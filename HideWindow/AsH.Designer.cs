1748dbc16496687bcf8bdb1d662966b844db0587
encodeous <47889571+encodeous@users.noreply.github.com>
2019-12-29 20:19:47 -0500

    Add files via upload

---
+++ HideWindow/AsH.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace HideWindow
+{
+    partial class AsH
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AsH));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.terminateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "AsH Tray Icon";
+            this.notifyIcon1.BalloonTipTitle = "AsH";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "AsH";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem,
+            this.terminateToolStripMenuItem,
+            this.showToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(128, 70);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // terminateToolStripMenuItem
+            // 
+            this.terminateToolStripMenuItem.Name = "terminateToolStripMenuItem";
+            this.terminateToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.terminateToolStripMenuItem.Text = "Terminate";
+            this.terminateToolStripMenuItem.Click += new System.EventHandler(this.terminateToolStripMenuItem_Click);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Hide);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Window";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(21, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 186);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(121, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 20);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Reload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Reload);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(221, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 20);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Show";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Show);
+            // 
+            // AsH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 333);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "AsH";
+            this.Text = "AsH";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem terminateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ HideWindow/AsH.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HideWindow
+{
+    public partial class AsH : Form
+    {
+        string curItem = null;
+        int hWnd = 0;
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+        [DllImport("User32")]
+        private static extern int ShowWindow(int hwnd, int nCmdShow);
+        //WORKING
+        public AsH()
+        {
+            InitializeComponent();
+        }
+
+        private void Hide(object sender, EventArgs e)
+        {
+            if (curItem == null)
+            {
+                MessageBox.Show("Please Choose a Window to hide");
+            }
+            else
+            {
+                Process[] processRunning = Process.GetProcesses();
+                foreach (Process pr in processRunning)
+                {
+                    if (pr.ProcessName == curItem)
+                    {
+                        hWnd = pr.MainWindowHandle.ToInt32();
+                        ShowWindow(hWnd, SW_HIDE);
+                    }
+                }
+            }
+
+        }
+
+        private void Show(object sender, EventArgs e)
+        {
+            if (hWnd != 0)
+            {
+                ShowWindow(hWnd, SW_SHOW);
+                hWnd = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process process in processlist)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    listBox1.Items.Add(process.MainWindowTitle);
+                }
+            }
+        }
+
+
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void terminateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Process[] prs = Process.GetProcesses();
+            foreach (Process pr in prs)
+            {
+                if (pr.ProcessName == curItem)
+                {
+
+                    pr.Kill();
+
+                }
+
+            }
+            Environment.Exit(0);
+        }
+        string ChildName2 = Process.GetCurrentProcess().ProcessName;
+        int hWnd2 = 0;
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Process[] processRunning = Process.GetProcesses();
+                foreach (Process pr in processRunning)
+                {
+                    if (pr.ProcessName == ChildName2)
+                    {
+                        hWnd2 = pr.MainWindowHandle.ToInt32();
+                        ShowWindow(hWnd2, SW_HIDE);
+                    }
+                }
+            }
+        }
+        private void showToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (hWnd2 != 0)
+            {
+                ShowWindow(hWnd2, SW_SHOW);
+                hWnd2 = 0;
+            }
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string title = listBox1.SelectedItem.ToString();
+            Process[] processlist = Process.GetProcesses();
+            bool windowfound= false;
+            foreach (Process process in processlist)
+            {
+                if (String.Equals(process.MainWindowTitle, title))
+                {
+                    curItem = process.ProcessName;
+                    windowfound = true;
+                }
+                else
+                {
+
+                }
+            }
+            if (windowfound)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Window not found!");
+                listBox1.Items.Clear();
+                Process[] processlist2 = Process.GetProcesses();
+
+                foreach (Process process2 in processlist2)
+                {
+                    if (!String.IsNullOrEmpty(process2.MainWindowTitle))
+                    {
+                        listBox1.Items.Add(process2.MainWindowTitle);
+                    }
+                }
+            }
+        }
+    
+
+    private void Reload(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process process in processlist)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    listBox1.Items.Add(process.MainWindowTitle);
+                }
+            }
+        }
+    }
+}
